a001e36d68931e72306650bf5cb15a96ceb04403
Elle Krogh <krogis01@luther.edu>
2014-11-02 15:28:29 -0600

    organize files

---
+++ js/app.js
@@ -0,0 +1,9 @@
+//Download AngularJS (http://angularjs.org/) - need angular.min.js
+//Download Twitter Bootstrap (http://getbootstrap.com/) - need bootstrap.min.css
+
+//<link rel="stylesheet" type="text/css" href="bootstrap.min.css"/>
+//<script type="text/javascript" src="angular.min.js"></script>
+
+//http://docs.angularjs.org/guide/expression
+
+var app = angular.module('store', []);
